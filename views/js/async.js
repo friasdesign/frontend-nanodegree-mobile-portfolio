8adf6afe6a7ababb5828b5dceb572b9b9a76039a
Carlos Frias <carlos.a.frias@gmail.com>
2016-08-04 16:57:11 -0300

    Refactor: Add comments to singletons in async.js

--- views/js/async.js
+++ views/js/async.js
@@ -1,20 +1,65 @@
+/*
+  `async.js` covers all logic that is not crucial for rendering content this is:
+    - Resizing pizzas using the slider.
+    - Loading pizzas in background.
+    - Move pizzas in background at scrolling.
+*/
+
 // PIZZA RESIZER _______________________________________________________________
 /*
-  Pizza Resizer is a singleton obj that wraps all functions neede for changing
+  Pizza Resizer is a singleton obj that wraps all functions needed for changing
   pizza size, since constantly defining functions and variables each time the
   event listener is triggered is not performant. We create a single instance of
   this object while page is loading.
   Defining most of these helper function on the global scope will pollute it, so
   they get wrapped inside this singleton.
 */
+
+/**
+ * A `PizzaResizerInstanciator` is used to create a unique instance of
+ * `PizzaResizer`, if called more than once, the same instance is returned.
+ * 
+ * @class PizzaResizerInstanciator
+ * @static
+ */
 var PizzaResizer = (function PizzaResizerInstanciator() {
+  /**
+   * Instance of `PizzaResizer` to be returned
+   *
+   * @property instance
+   * @type PizzaResizer
+   */
   var instance;
 
+  /**
+   * `PizzaResizer` object is a singleton object used to deal with all logic
+   * related to resizing pizzas when slider changes. Wrapping all these
+   * functions inside a singleton pattern prevents global scope pollution.
+   *
+   * @class PizzaResizer
+   * @static
+   */
   function PizzaResizer() {
-    // Private props and methods
+    /**
+     * This variable holds the container of all random pizzas identified by id
+     * `randomPizzas`.
+     *
+     * @property randomPizzas
+     * @type Array
+     * @private
+     */
     var randomPizzas = document.getElementById('randomPizzas');
     
-    // Changes the slider value to a percent width
+    /**
+     * This method takes a size and returns a function to be executed based on
+     * the kind of resizing to do. Those are: `resizeSmall`, `resizeMedium`,
+     * `resizeLarge`.
+     * 
+     * @method sizeSwitcher
+     * @private
+     * @param  size {String} A size to be matched. It can be 1, 2 or 3.
+     * @return {function} A function that resizes randomPizzaContainer
+     */
     function sizeSwitcher (size) {
       var classBase = 'randomPizzaContainer',
           classLarge = classBase + '--large',
@@ -22,18 +67,39 @@ var PizzaResizer = (function PizzaResizerInstanciator() {
 
       switch(size) {
         case "1":
+          /**
+           * `resizeSmall` function is used when the user moves the slider to
+           *  the __small__ position. It assigns `classSmall` to the `classList`
+           *  of `DOMNode` and removes other previous class assignments.
+           * 
+           * @param  DOMNode {Element}
+           */
           return function resizeSmall(DOMNode) {
             var classList = DOMNode.classList;
             classList.remove(classLarge);
             classList.add(classSmall);
           };
         case "2":
+          /**
+           * `resizeMedium` function is used when the user moves the slider to
+           *  the __medium__ position. It removes any previous class assignment
+           *  to `DOMNode` defaulting its size.
+           * 
+           * @param  DOMNode {Element}
+           */
           return function resizeMedium(DOMNode) {
             var classList = DOMNode.classList;
             classList.remove(classLarge);
             classList.remove(classSmall);
           };
         case "3":
+          /**
+           * `resizeLarge` function is used when the user moves the slider to
+           *  the __large__ position. It assigns `classLarge` to the `classList`
+           *  of `DOMNode` and removes other previous class assignments.
+           * 
+           * @param  DOMNode {Element}
+           */
           return function resizeLarge(DOMNode) {
             var classList = DOMNode.classList;
             classList.remove(classSmall);
@@ -44,7 +110,14 @@ var PizzaResizer = (function PizzaResizerInstanciator() {
       }
     }
 
-    // Changes the value for the size of the pizza above the slider
+    /**
+     * This method changes the text in slider's label according to the user's
+     * input.
+     * 
+     * @method changeSliderLabel
+     * @private
+     * @param  size {String} A size to be matched. It can be 1, 2 or 3.
+     */
     function changeSliderLabel(size) {
       var slider = document.getElementById("pizzaSize");
       switch(size) {
@@ -63,7 +136,13 @@ var PizzaResizer = (function PizzaResizerInstanciator() {
     }
 
     return {
-      // Iterates through pizza elements on the page and changes their widths
+      /**
+       * This method iterates through pizza elements on the page and changes
+       * their widths.
+       * 
+       * @method changePizzaSizes
+       * @param  size {String} A size to be matched. It can be 1, 2 or 3.
+       */
       changePizzaSizes: function (size) {
         var randomContainers = document.getElementsByClassName('randomPizzaContainer'),
             changingFunction = sizeSwitcher(size);
@@ -77,6 +156,15 @@ var PizzaResizer = (function PizzaResizerInstanciator() {
   }
 
   return {
+    /**
+     * It returns a new instance of `PizzaResizer` if no instance exists, if
+     * already instanciated, a reference to that instance is returned instead.
+     * 
+     * @method getInstance
+     * @for PizzaResizerInstanciator
+     * @return {Object} A reference to a new or already existing instance of
+     * `PizzaResizer`.
+     */
     getInstance: function() {
       if(!instance) {
         instance = PizzaResizer();
@@ -85,20 +173,64 @@ var PizzaResizer = (function PizzaResizerInstanciator() {
     }
   };
 })();
+// --> END PIZZARESIZER ________________________________________________________
 
+/**
+ * A `MoverInstanciator` is used to create a unique instance of
+ * `Mover`, if called more than once, the same instance is returned.
+ * 
+ * @class MoverInstanciator
+ * @static
+ */
 var Mover = (function MoverInstanciator(){
+  /**
+   * Instance of `Mover` to be returned
+   *
+   * @property instance
+   * @type Mover
+   */
   var instance;
 
+  /**
+   * `Mover` object is a singleton object used to deal with all logic
+   * related to moving pizzas in background when user scrolls.
+   *
+   * @class Mover
+   * @static
+   */
   function Mover() {
-    // Iterator for number of times the pizzas in the background have scrolled.
-    // Used by updatePositions() to decide when to log the average time per frame
+    /**
+     * Iterator for number of times the pizzas in the background have scrolled.
+     * Used by `updatePositions()` to decide when to log the average time per frame.
+     *
+     * @property frame
+     * @type Integer
+     * @default 0
+     * @private
+     */
     var frame = 0;
 
     // This variable holds references for .mover elements.
+    /**
+     * This property holds references for .mover elements.
+     *
+     * @property movers
+     * @type Array
+     * @default []
+     * @private
+     */
     var movers = [];
 
-    // Logs the average amount of time per 10 frames needed to move the sliding background pizzas on scroll.
-    function logAverageFrame(times) {   // times is the array of User Timing measurements from updatePositions()
+    // 
+    /**
+     * This method logs the average amount of time per 10 frames needed to move
+     * the sliding background pizzas on scroll.
+     *
+     * @method logAverageFrame
+     * @private
+     * @param  times {Array} the array of User Timing measurements from updatePositions().
+     */
+    function logAverageFrame(times) {   // times is 
       var numberOfEntries = times.length;
       var sum = 0;
       for (var i = numberOfEntries - 1; i > numberOfEntries - 11; i--) {
@@ -108,10 +240,13 @@ var Mover = (function MoverInstanciator(){
     }
 
     return {
-      // The following code for sliding background pizzas was pulled from Ilya's demo found at:
-      // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
-
-      // Moves the sliding background pizzas based on scroll position
+      /**
+       * This method moves the sliding background pizzas based on scroll position.
+       * This code was pulled from
+       * [Ilya's demo](https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html)
+       * 
+       * @method updatePositions
+       */
       updatePositions: function () {
         var bodyScrollTop = document.body.scrollTop / 1250;
         frame++;
@@ -132,9 +267,16 @@ var Mover = (function MoverInstanciator(){
         }
       },
 
+      /**
+       * This method loads the pizzas in background
+       * 
+       * @method initialLoad
+       */
       initialLoad: function() {
         var cols = 8,
             s = 256,
+            // These variables are used to calculate how many pizzas are needed
+            // to fill the current viewport's height.
             numberOfRows = Math.round(window.screen.height / s),
             numberOfPizzas = numberOfRows * cols,
             elem;
@@ -156,8 +298,16 @@ var Mover = (function MoverInstanciator(){
     };
   }
 
-
   return {
+    /**
+     * It returns a new instance of `Mover` if no instance exists, if
+     * already instanciated, a reference to that instance is returned instead.
+     * 
+     * @method getInstance
+     * @for MoverInstanciator
+     * @return {Object} A reference to a new or already existing instance of
+     * `Mover`.
+     */
     getInstance: function() {
       if(!instance) {
         return Mover();
